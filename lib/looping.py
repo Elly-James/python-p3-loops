d793fd743a529d9352eafc19399acecb9d1a4074
Elly <ellyjames1999@gmail.com>
2025-02-11 22:44:50 +0300

    done all tests

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,45 @@
 #!/usr/bin/env python3
 
+
+
+# Write a function happy_new_year() using a while loop that outputs
+#  numbers starting at 10 and counting down to 1. After reaching 1, print out "Happy New Year!"
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i >= 1:
+        print(i)
+        i -= 1
+        if i < 1:
+            print("Happy New Year!")
+happy_new_year()
+
+    
+
+#Write a function square_integers() that takes one argument,
+#  a list of integers and returns the list of squared elements.
+
+    
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_list = []
+    for num in int_list:
+        squared_list.append(num**2)
+    return squared_list
+    
+
+
+# Write a function fizzbuzz() that prints the numbers from 1 to 100.
+#  For multiples of three, print "Fizz" instead of the number and for the multiples of 
+# five print "Buzz". For numbers which are multiples of both three and five, print "FizzBuzz".
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+   
